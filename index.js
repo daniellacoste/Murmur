41db02e339c7098091753e37d60d192701cf0ba0
delacost <daniel.e.lacoste@gmail.com>
2017-03-09 13:58:11 -0700

    updates to index.html/js & styling

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,23 +14,71 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/'));
 
+var users = [];
+var connections = [];
+
 io.on('connection', function(socket){
+  console.log('A user connected');
+  connections.push(socket); // add socket to array of connections
+  console.log('Connected: %s sockets connected', connections.length);
 
-  socket.on('connect', function(){
-   console.log('a user connected');
+
+  // Set username
+  socket.on('setUsername', function(userid){
+    userid = randUsername(userid);
+    if(users.indexOf(userid) > -1){
+      socket.emit('userExists', userid);
+    }
+    else{
+      socket.userid = userid;
+      users.push(socket.userid);
+      updateUsernames();
+      socket.emit('newUser', {username: userid});
+      console.log(users);
+    }
+  });
+  
+  function updateUsernames() {
+    io.sockets.emit('getUsers', users);
+  };
+
+  // Disconnect
+  socket.on('disconnect', function(data, userid){
+    users.splice(users.indexOf(socket.userid), 1);
+    console.log('User disconnected');
+    connections.splice(connections.indexOf(socket), 1); // remove socket from connections
+
+    console.log('Disconnected: %s sockets connected', connections.length);
+    io.sockets.emit('getUsers', users); // update the list of connected users
+    
+    if(users.indexOf(userid > -1)){
+      users.pop(userid);
+      console.log(users);
+    }
   });
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  socket.on('msg', function(msg, userid){
+    console.log('Message: ' + msg);
+    io.sockets.emit('msg', '[' + getUTC() + ']: ' + msg);
   });
 
-  socket.on('disconnect', function(){
-   console.log('user disconnected');
+  io.of('/').clients(function(error, clients){
+    if (error) throw error;
+    console.log(clients); 
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
+// Calculate the current time
+function getUTC(){
+  var d = new Date();
+  var dateStr = d.toUTCString();
+  return dateStr;
+};
+
+// Generate a random userid b/w 1-1000
+function randUsername(data){
+  data = Math.floor((Math.random() * 1000)+1);
+  return data;
+};
 
