56660ceb4c4cf5bcc4922c6811a9267f26dcc227
delacost <daniel.e.lacoste@gmail.com>
2017-03-10 20:59:37 -0700

    fixed some nickname bugs and cleaned up code

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ var connections = [];
 var messageHistory = []; 
 
 io.on('connection', function(socket){
-  console.log('A user connected');
+  console.log('A socket connected');
   connections.push(socket); // add socket to array of connections
   console.log('Connected: %s sockets connected', connections.length);
 
-  // Set username
+  // Generate a random username 
   socket.on('setUsername', function(userid){
     userid = randUsername();
     if (users.indexOf(userid) > -1){
@@ -39,16 +39,17 @@ io.on('connection', function(socket){
       console.log('Connected users: ', users);
     }
   });
- 
+
+  // Broadcast the users array 
   function updateUsernames(){
     io.sockets.emit('getUsers', users);
   };
 
-  // Disconnect
+  // Disconnect user & socket
   socket.on('disconnect', function(data, userid){
-    removeUser(userid);
+    disconnectUser(userid);
     console.log('User %s has disconnected', socket.userid);
-    io.sockets.emit('getUsers', users); // update the list of connected users
+    io.sockets.emit('getUsers', users);
     console.log('Connected users: ', users);
     connections.splice(connections.indexOf(socket), 1); // remove socket from connections
     console.log('Disconnection: %s sockets connected', connections.length);
@@ -57,22 +58,21 @@ io.on('connection', function(socket){
   // Sending messages to other clients
   socket.on('msg', function(msg, userid){
     console.log('Message: "' + msg + '" from user: ' + socket.userid);
-
     if (msg.substring(0,5) === "/nick"){
       var nickVal = msg.substring(6);
       if (users.indexOf(nickVal) > -1){
         socket.emit('nickExists', nickVal);
       }
       else{
-        removeUser(userid);
+        var payload = {timestamp: getUTC(), userid: '', color: 'black', message: "User " + socket.userid + " changed their nickname to: " + nickVal}; // color function
+        messageHistory.push(payload);
+        io.sockets.emit('msg', payload);
+        disconnectUser(userid);
         setNick(nickVal);
       }
-//      var payload = {timestamp: getUTC(), userid: nickVal, color: 'black', message: msg}; // color function
-//      io.sockets.emit('msg', payload);
-//      messageHistory.push(payload); // log msg into chat history 
     } 
 
-    if (msg.substring(0,10) === "/nickcolor"){
+    else if (msg.substring(0,10) === "/nickcolor"){
       var colorVal = msg.substring(12);
       var payload = {timestamp: getUTC(), userid: socket.userid, color: colorVal, message: msg}; // color function
       io.sockets.emit('msg', payload);
@@ -94,22 +94,17 @@ io.on('connection', function(socket){
   }
 
   function setNick(userid){ 
-    console.log(userid);
-    if (users.indexOf(userid) > -1){
-      socket.emit('userExists', userid);
-    }
-    else{
-      socket.userid = userid;
-      users.push(socket.userid);
-      updateUsernames();
-      socket.emit('newUser', {username: userid});
-      // Print individual messages that exists in the history array
-      console.log(users);
-    }
+    console.log('Nickname %s set by user %s', userid, socket.userid);
+    socket.userid = userid;
+    users.push(socket.userid);
+    updateUsernames();
+    socket.emit('newUser', {username: userid});
+    console.log('Connected users: ', users);
   }
-    
-  function removeUser(userid){
-    var dUser = users.splice(users.indexOf(socket.userid), 1); // disconnect userid
+
+  // Remove userid from users array 
+  function disconnectUser(userid){
+    var dUser = users.splice(users.indexOf(socket.userid), 1);
     return dUser;
   }
 
@@ -131,4 +126,3 @@ function randUsername(){
   var data = Math.floor((Math.random() * 10)+1);
   return data;
 };
-
